c4dc3d2e4e2ada7afdf071ee713e619e2521f111
Rohith-futurecalls <rohith@futurecalls.com>
2021-05-28 20:54:34 +0530

    Added 404 login

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ http
                   "Content-Type": "text/html",
 
               });
+		    console.log("404 page has been visited
               response.write("<p>404 Page Not found!</p>");
               response.end();
               break;
